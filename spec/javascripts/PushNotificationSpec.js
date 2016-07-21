6f9a2441879295a13c8347f7819d7f3c591a07d9
asmsuechan <suenagaryoutaabc@gmail.com>
2016-07-21 22:28:02 +0900

    Remove extra spec dir

--- spec/javascripts/PushNotificationSpec.js
+++
@@ -1,9 +0,0 @@
-describe('instantiation', function() {
-
-    'use strict';
-
-    it('should create a new Notify instance', function() {
-        var notification = window.$.notify("body");
-        expect(notification instanceof window.Notification).toBeTruly();
-    });
-});
